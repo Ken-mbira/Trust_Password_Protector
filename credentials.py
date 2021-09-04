9a2bbc452fa95c9e277950f75430523c13756acb
ken <ken.mbira@student.moringaschool.com>
2021-09-04 10:29:26 +0300

    add a test for creating a new credential

--- credentials.py
+++ credentials.py
@@ -1,5 +1,16 @@
+from user import User
+
+
 class Cred:
     """
     This is a class that makes the user credentials for their different accounts
     """
-    pass
+    
+    def __init__(self,account_name,user_name,email,password):
+        """
+        This will construct an instance of the credentials class
+        """
+        self.account_name = account_name
+        self.user_name = user_name
+        self.email = email
+        self.password = password
--- test_credentials.py
+++ test_credentials.py
@@ -7,3 +7,21 @@ class Test_Credentials(unittest.TestCase):
     Args: 
         unittest.TestCase: This is a class of the unittest module that will bring some testing features
     """
+
+    def setUp(self):
+        """
+        This is to be run before every test is carried out
+        """
+        self.new_credential = Cred("Insta","mbira","mbiraken@insta.com","1234")
+
+    def test__init(self):
+        """
+        This will check if one an create a new credential
+        """
+        self.assertEqual(self.new_credential.account_name,"Insta")
+        self.assertEqual(self.new_credential.email,"mbiraken@insta.com")
+        self.assertEqual(self.new_credential.password,"1234")
+        self.assertEqual(self.new_credential.user_name,"mbira")
+
+if __name__ == "__main__":
+    unittest.main()
