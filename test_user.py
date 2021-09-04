ee152c73883ce18bfda66943b1c08b96ee65cd28
ken <ken.mbira@student.moringaschool.com>
2021-09-04 06:15:59 +0300

    add a test for whether one can create a new user

--- test_user.py
+++ test_user.py
@@ -7,3 +7,10 @@ class User_Tests(unittest.TestCase):
         The setup to run before every test case
         """
         self.new_user = User("Ken","1234")
+
+    def user_generate(self):
+        self.assertEqual(self.new_user.user_name, "Ken")
+        self.assertEqual(self.new_user.password,"1234")
+
+if __name__ == "__main__":
+    unittest.main()
