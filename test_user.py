9a7e2b64a59f3b0f29437cce35fed443c6bf5184
ken <ken.mbira@student.moringaschool.com>
2021-09-04 06:22:47 +0300

    add a method to add a user into the user list and also a test for the same

--- test_user.py
+++ test_user.py
@@ -8,9 +8,16 @@ class User_Tests(unittest.TestCase):
         """
         self.new_user = User("Ken","1234")
 
-    def user_generate(self):
+    def user_generate_test(self):
         self.assertEqual(self.new_user.user_name, "Ken")
         self.assertEqual(self.new_user.password,"1234")
+
+    def user_save_test(self):
+        """
+        This is to test whether a user can be saved into the list of users
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
 
 if __name__ == "__main__":
     unittest.main()
--- user.py
+++ user.py
@@ -3,10 +3,16 @@ class User:
     Class that generates a new account for a user
     to store passwords
     """
-    user_Array = [] #The arrar that multiple users will be saved
+    user_list = [] #The arrar that multiple users will be saved
 
     def __init__ (self,user_name,password):
         """
         The method that creates an instance of the user class"""
         self.user_name = user_name
         self.password = password
+
+    def save_user(self):
+        """
+        This is to save a new user into the list of users
+        """
+        User.user_list.append(self)
