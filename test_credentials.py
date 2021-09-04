910280d1e410a926d2610f50f5fd39c2caaeb8e6
ken <ken.mbira@student.moringaschool.com>
2021-09-04 10:45:20 +0300

    add a test for creating multiple credentials

--- test_credentials.py
+++ test_credentials.py
@@ -23,12 +23,27 @@ class Test_Credentials(unittest.TestCase):
         self.assertEqual(self.new_credential.password,"1234")
         self.assertEqual(self.new_credential.user_name,"mbira")
 
+    def tearDown(self):
+        """
+        This will run after every test runs and clear up the code
+        """
+        Cred.credential_list = []
+
     def test_save_cred(self):
         """
         This will test on whether we can add a credential to the credentials list
         """
         self.new_credential.save_credential()
         self.assertEqual(len(Cred.credential_list),1)
+
+    def test_multiple_save(self):
+        """
+        This will test whether we can save multiple credentials
+        """
+        self.new_credential.save_credential()
+        other_credential = Cred("Twitter","Mbira","mbira@ken.com","1234")
+        other_credential.save_credential()
+        self.assertEqual(len(Cred.credential_list),2)
 
 if __name__ == "__main__":
     unittest.main()
